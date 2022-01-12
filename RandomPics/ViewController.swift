cfb11cf845efac886c9e476ea198aca9c013c939
tristiino <73903495+tristiino@users.noreply.github.com>
2022-01-12 00:49:32 -0500

    completion

--- RandomPics/ViewController.swift
+++ RandomPics/ViewController.swift
@@ -9,11 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .black
+        return imageView
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .black
+        button.setTitle("Random Pictures", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
+        // image view
+        view.addSubview(imageView)
+        imageView.frame = CGRect(
+            x: 0,
+            y: 0,
+            width: 300,
+            height: 300)
+        imageView.center = view.center
+        // button
+        view.addSubview(button)
+        
+        getRandomPhoto()
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
-
+    @objc func didTapButton()  {
+        getRandomPhoto()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        button.frame = CGRect (
+            x: 30,
+            y: view.frame.size.height-150-view.safeAreaInsets.bottom,
+            width: view.frame.size.width-60,
+            height: 60)
+    }
+    
+    
+    func getRandomPhoto()   {
+        let urlString = "https://source.unsplash.com/random.600x600"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
+    }
 
 }
 
